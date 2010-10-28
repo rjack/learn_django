dcfaf74a6fd3452d199e77e76ee15079ff5dacf1
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-10-28 15:41:11 +0200

    register Choice as Poll inline into the admin site

--- learning/polls/admin.py
+++ learning/polls/admin.py
@@ -1,4 +1,21 @@
-from polls.models import Poll
+from polls.models import Poll, Choice
 from django.contrib import admin
 
-admin.site.register(Poll)
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+
+# You'll follow this pattern -- create a model admin object, then pass it as
+# the second argument to admin.site.register() -- any time you need to change
+# the admin options for an object.
+class PollAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,               {'fields': ['question']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+
+
+admin.site.register(Poll, PollAdmin)
