8a1de021f26490736d695d39760b0d775586f0a0
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-10-28 16:54:36 +0200

    polls views placeholders

--- learning/polls/views.py
+++ learning/polls/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Hello, world. You're at the poll index.")
+
+
+def detail(request, poll_id):
+	return HttpResponse("Poll %s" % poll_id)
+
+
+def results(request, poll_id):
+	return HttpResponse("Results for poll %s" % poll_id)
+
+
+def vote(request, poll_id):
+	return HttpResponse("Vote on poll %s" % poll_id)
