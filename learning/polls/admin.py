714fa1f2f938439ec16616851c935dec65edaa1c
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-10-28 16:02:15 +0200

    PollAdmin: display fields, filters, search, date hierarchy, short description

--- learning/polls/admin.py
+++ learning/polls/admin.py
@@ -16,6 +16,10 @@ class PollAdmin(admin.ModelAdmin):
 		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
 	inlines = [ChoiceInline]
+	list_display = ('question', 'pub_date', 'was_published_today')
+	list_filter = ['pub_date']
+	search_fields = ['questions']
+	date_hierarchy = 'pub_date'
 
 
 admin.site.register(Poll, PollAdmin)
