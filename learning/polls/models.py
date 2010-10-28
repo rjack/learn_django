32c0db039bd7aa05e14cbaf2011f8a6c3ee8a96b
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-10-28 14:49:08 +0200

    Poll: was_published_today method

--- learning/polls/models.py
+++ learning/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
@@ -8,6 +9,9 @@ class Poll(models.Model):
 
 	def __unicode__(self):
 		return self.question
+
+	def was_published_today(self):
+		return self.pub_date.date() == datetime.date.today()
 
 
 class Choice(models.Model):
