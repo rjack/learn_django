27fb6efb39ffef829b191a58fefad849b0e9518c
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-10-28 15:29:27 +0200

    admin site enabled

--- learning/settings.py
+++ learning/settings.py
@@ -95,7 +95,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'polls',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
