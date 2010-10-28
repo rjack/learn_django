9956ca20b9cc9e66bb7e488337d7dd6701dc9898
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-10-28 16:54:48 +0200

    polls urls

--- learning/urls.py
+++ learning/urls.py
@@ -5,12 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^learning/', include('learning.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+	(r'^polls/$', 'polls.views.index'),
+	(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+	(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+	(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+	(r'^admin/', include(admin.site.urls)),
 )
